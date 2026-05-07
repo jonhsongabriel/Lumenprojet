36ec5c02203d1083c5721f6f8ce86ebe5e429095
Jonhsongabriel <jonhsonratsimbaherison@gmail.com>
2026-05-07 11:09:17 +0300

    fix backend index.js + connect-device API

---
+++ lumen-backend/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+const lumenRoutes = require("./routes/lumen.routes");
+app.use("/api/lumen", lumenRoutes);
+
+// TEST API
+app.get("/", (req, res) => {
+  res.json({ status: "API Lumen running" });
+});
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
