422397e3d307d0a629d304386f8ebe21f845d6ba
Jonhsongabriel <jonhsonratsimbaherison@gmail.com>
2026-05-08 10:55:58 +0300

    Correction des route

--- lumen-backend/routes/lumen.routes.js
+++ lumen-backend/routes/lumen.routes.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const { connectDevice } = require("../controllers/lumen.controller");
 
+// CONNECT DEVICE
 router.post("/connect-device", connectDevice);
+
+// PROJETS (IMPORTANT)
+router.get("/projets", (req, res) => {
+  res.json([]);
+});
 
 module.exports = router;
