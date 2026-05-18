38def75dffb8c4516b6d343bf9bc72277391817a
Jonhsongabriel <jonhsonratsimbaherison@gmail.com>
2026-05-18 16:55:34 +0300

    moddif server

--- lumen-backend/models/index.js
+++ lumen-backend/models/index.js
@@ -6,11 +6,7 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// ❌ NE PAS APPELER COMME UNE FONCTION
-db.Client = require("./Client");
-db.Projet = require("./Projets");
-db.Administrateur = require("./Administrateur");
-db.Demande = require("./Demande");
-db.User = require("./User");
+// IMPORTANT
+db.Projet = require("./Projets")(sequelize, Sequelize.DataTypes);
 
 module.exports = db;
